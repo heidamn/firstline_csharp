27a22cc3dfe0a538f9e233d93bf71c1668d2f19c
heidamn <heidamn@gmail.com>
2019-11-09 10:37:13 -0100

    1 лаба

---
+++ Lab1/Lab1/PhoneBookPage.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab1
+{
+    class PhoneBookPage
+    {
+        private static int lastId = 0;
+        public int Id { get; private set; }
+        private string firstName;
+        private string secondName; // необязательно
+        private string lastName;
+        private long phoneNumber;
+        private string country;
+        private DateTime bDay; // необязательно
+        private string organisation; //необязательно
+        private string position; //необязательно
+        private string notes; //необязательно
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                while (value == "")
+                {
+                    Console.WriteLine("Поле не может быть пустым");
+                    value = Console.ReadLine();
+                }
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                while (value == "")
+                {
+                    Console.WriteLine("Поле не может быть пустым");
+                    value = Console.ReadLine();
+                }
+                lastName = value;
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber.ToString(); }
+            set
+            {
+
+                while (value == "" || !value.All(char.IsDigit))
+                {
+                    Console.WriteLine("Поле не может быть пустым и должно содержать только числа");
+                    value = Console.ReadLine();
+                }
+                phoneNumber = long.Parse(value);
+            }
+        }
+
+        public string Country
+        {
+            get { return Country; }
+            set
+            {
+                while (value == "")
+                {
+                    Console.WriteLine("Поле не может быть пустым");
+                    value = Console.ReadLine();
+                }
+                country = value;
+            }
+        }
+
+        public string BDay
+        {
+            get 
+            {
+                if (bDay == DateTime.Parse("01.01.0001"))
+                {
+                    return "Не задана";
+                }
+                else
+                {
+                    return bDay.ToShortDateString();
+                }
+            }
+            set
+            {
+                if (value == "")
+                {
+                    bDay = DateTime.Parse("01.01.0001");
+                }
+                else
+                {
+                    while (!DateTime.TryParse(value, out bDay))
+                    {
+                        Console.WriteLine("Неверный формат, попробуйте снова.");
+                        value = Console.ReadLine();
+                    }
+                }
+            }
+        }
+
+
+        public PhoneBookPage()
+        {
+            this.Id = lastId;
+            EditPage();
+            lastId++;
+            Console.WriteLine("Страница создана!"); 
+        }
+        
+        //редактирование
+        public void EditPage() 
+        {
+            Console.WriteLine("Введите имя:");
+            FirstName = Console.ReadLine();
+            Console.WriteLine("Введите отчество: \n или нажмите Enter, чтобы продолжить");
+            secondName = Console.ReadLine();
+            Console.WriteLine("Введите фамилию:");
+            LastName = Console.ReadLine();
+            Console.WriteLine("Введите номер телефона (число):");
+            PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Введите страну:");
+            Country = Console.ReadLine();
+            Console.WriteLine("Введите дату рождения: \n  или нажмите Enter, чтобы продолжить");
+            BDay = Console.ReadLine();
+           
+            Console.WriteLine("Введите компанию: \n  или нажмите Enter, чтобы продолжить");
+            organisation = Console.ReadLine();
+            Console.WriteLine("Введите должность: \n  или нажмите Enter, чтобы продолжить");
+            position = Console.ReadLine();
+            Console.WriteLine("Введите заметки: \n  или нажмите Enter, чтобы продолжить");
+            notes = Console.ReadLine();
+            
+        }
+        
+       
+        public override string ToString()
+        {
+            return $"id: {this.Id}\n{this.lastName} {this.firstName}\n{this.phoneNumber}";
+        }
+        public void FullInfo()
+        {
+            Console.WriteLine($"id: {Id}");
+            if (secondName == "")
+            {
+                Console.WriteLine($"{firstName} {lastName}");
+            }
+            else
+            {
+                Console.WriteLine($"{FirstName} {secondName} {LastName}");
+            }
+                Console.WriteLine($"{phoneNumber}");
+                Console.WriteLine($"{country}");
+            if (bDay != DateTime.Parse("01.01.0001"))
+            {
+                Console.WriteLine($"Дата рождения: {BDay}");
+            }
+            if (organisation != "")
+            {
+                Console.WriteLine($"Организация: {organisation}");
+            }
+            if (position != "")
+            {
+                Console.WriteLine($"Должность: {position}");
+            }
+            if (notes != "")
+            {
+                Console.WriteLine($"Заметки: {notes}");
+            }
+        }
+
+
+            
+
+    }
+}
---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab1
+{
+    class PhoneBook
+    {   
+        public static List<PhoneBookPage> phoneBook = new List<PhoneBookPage>();
+        public static string helpCommand = "команды \n create - создание записи \n edit id - редактирование пользователя с id \n delete id - удаление пользователя с id \n info id - просмотр полной и информации о пользователе \n allInfo - информация о всех пользователях \n clear - очистка консоли \n help - список команд \n exit - выход";
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Добро пожаловать, введите команду, чтобы начать! (help для получения списка команд)");
+            while (true)
+            {
+                Console.Write(">");
+                string command = Console.ReadLine();
+                if (command == "exit")
+                {
+                    Console.WriteLine("Удачи! Все файлы были удалены, хе.");
+                    break;
+                }
+                else if (command == "help")
+                {
+                    Console.WriteLine(helpCommand);
+                }
+                else if (command == "allInfo")
+                {
+                    PhoneBook.ViewAll();
+                }
+                else if (command == "create")
+                {
+                    PhoneBookPage page = new PhoneBookPage();
+                    phoneBook.Add(page);
+                }
+                else
+                {
+                    string[] commands = command.Split(" ");
+                    if (commands[0] == "edit")
+                    {
+                        if (HaveId(commands))
+                        {
+                            for (int i = 0; i <= phoneBook.Count; i++)
+                            {
+                                if (!int.TryParse(commands[1], out _))
+                                {
+                                    Console.WriteLine("id должен быть числом!");
+                                    break;
+                                }
+                                else if (i == phoneBook.Count)
+                                {
+                                    Console.WriteLine("id не существует!");
+                                }
+                                else if (phoneBook[i].Id == int.Parse(commands[1]))
+                                {
+                                    phoneBook[i].EditPage();
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    else if (commands[0] == "delete")
+                    {
+                        if (HaveId(commands))
+                        {
+                            for (int i = 0; i <= phoneBook.Count; i++)
+                            {
+                                if (!int.TryParse(commands[1], out _))
+                                {
+                                    Console.WriteLine("id должен быть числом!");
+                                    break;
+                                }
+                                else if (i == phoneBook.Count)
+                                {
+                                    Console.WriteLine("id не существует!");
+                                }
+                                else if (phoneBook[i].Id == int.Parse(commands[1]))
+                                {
+                                    Console.Write($"Чтобы удалить запись с id {commands[1]}, введите Y, чтобы отменить, нажмите Enter: ");
+                                    if ("Y" == Console.ReadLine())
+                                    {
+                                        phoneBook.Remove(phoneBook[i]);
+                                        Console.WriteLine("Запись удалена");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Удаление отменено");
+                                    }
+                                    break;
+
+                                }
+                            }
+                        }
+                    }
+                    else if (commands[0] == "info")
+                    {
+                        if (HaveId(commands))
+                        {
+                            for (int i = 0; i <= phoneBook.Count; i++)
+                            {
+                                if (!int.TryParse(commands[1], out _))
+                                {
+                                    Console.WriteLine("id должен быть числом!");
+                                    break;
+                                }
+                                else if (i == phoneBook.Count)
+                                {
+                                    Console.WriteLine("id не существует!");
+                                }
+                                else if (phoneBook[i].Id == int.Parse(commands[1]))
+                                {
+                                    phoneBook[i].FullInfo();
+                                    break;
+                                }
+                            }
+
+                        }
+                    }
+                    else if (commands[0] == "clear") 
+                    {
+                        Console.Clear();
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Команды не существует, попробуйте еще раз");
+                    }
+                }
+            }
+        }
+
+        private static bool HaveId(string[] commands)
+        {
+            if (commands.Length == 1)
+            {
+                Console.WriteLine("Введите id");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        static void ViewAll()
+        {
+            if (phoneBook.Count == 0)
+            {
+                Console.WriteLine("Записей нет!");
+            }
+            else
+            {
+                foreach (PhoneBookPage page in phoneBook)
+                {
+                    Console.WriteLine(page);
+                }
+            }
+        }
+    }
+}
